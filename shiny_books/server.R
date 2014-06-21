3ad581855bfe1cf23d01fd03bc858bcee226a34c
Shannon Rush <shannonmrush@gmail.com>
2014-06-21 08:53:10 -0600

    adding Rdata to summary document, adding monthadded to Rdata

--- shiny_books/server.R
+++ shiny_books/server.R
@@ -7,11 +7,10 @@ load("genre_data.Rdata")
 shinyServer(function(input, output) {
     output$plot <- renderPlot({
         selected.data <- subset(genre.data, genre %in% input$genres)
-        selected.data$dateadded <- floor_date(as.Date(selected.data$dateadded), "month")
         by_genre_and_date <- selected.data %.%
-            group_by(genre, dateadded) %.%
+            group_by(genre, monthadded) %.%
             summarise(counts=n())
-        p <- ggplot(by_genre_and_date, aes(x=dateadded, y=counts, color=genre, group=genre))
+        p <- ggplot(by_genre_and_date, aes(x=monthadded, y=counts, color=genre, group=genre))
         l <- p + xlab("Date Added")+ylab("Number Added")+ggtitle("Genre Popularity Over Time")
         t <- l + theme(plot.title = element_text(lineheight=40, size=20, face="bold", vjust=1),
                        axis.title = element_text(size=17),
